c90ba73c22a9b62020490b66c1c41ee0aebd5153
Kong Huang <kh1534@nyu.edu>
2015-09-19 15:30:00 -0400

    done

---
+++ week03/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	public Transform player;
+	public Text textUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color = textUI.material.color;
+		color.a = 1f;
+		textUI.material.color = color;
+		if (Input.GetKeyDown(KeyCode.Space) && (Vector3.Distance(transform.position, player.position) < 40f)){
+			textUI.text = "YOU WIN";
+		}
+	
+	}
+}
---
+++ week03/Assets/Scripts/HintSystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HintSystem : MonoBehaviour {
+
+	public Text textUI;
+	public Transform player;
+	public Transform goal;
+	public Transform thing;
+	public Transform chest;
+	public Transform log;
+	
+
+	// Update is called once per frame
+	void Update () {
+		textUI.text = "";
+		if ( (Vector3.Distance(new Vector3(-1.7f, 0f, -85f), player.position) < 40f) ){
+			textUI.text = "Welcome to the game, maybe head north and check near that random log ...";
+		}
+		if ( (Vector3.Distance(log.position, player.position) < 40f) ){
+			textUI.text = "Completely off. It's not anywhere near this log..." +
+						  "\nCheck the creepy rotating thing.";
+		}
+		if ( (Vector3.Distance(thing.position, player.position) < 40f) ){
+			textUI.text = "Go south from here. Maybe near that random treasure chest???";
+		}
+		if ( (Vector3.Distance(chest.position, player.position) < 40f) ){
+			textUI.text = "It's around here... Maybe it's in that pond over there?";
+		}
+	}
+}
