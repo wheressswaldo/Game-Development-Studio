b6b1125402e1751ed28e6d7f4a76896064d66a74
Kong Huang <kh1534@nyu.edu>
2015-09-23 19:44:20 -0400

    some preliminary work

---
+++ week04/Assets/Scripts/colorChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorChange : MonoBehaviour {
+	void OnCollisionEnter(Collision other) {
+		Debug.Log("Contact was made!");
+		if ( other.gameObject.CompareTag("Trigger") ){
+			transform.GetComponent<Renderer>().material.color = Color.red;
+		}
+	}
+}
