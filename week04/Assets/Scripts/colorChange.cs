b5b28f5043c68907f6a569fe2877df30914e44ce
Kong Huang <kh1534@nyu.edu>
2015-09-24 04:38:24 -0400

    some easy preliminary stuff
    easy stuff

--- week04/Assets/Scripts/colorChange.cs
+++ week04/Assets/Scripts/colorChange.cs
@@ -2,10 +2,9 @@
 using System.Collections;
 
 public class colorChange : MonoBehaviour {
-	void OnCollisionEnter(Collision other) {
-		Debug.Log("Contact was made!");
-		if ( other.gameObject.CompareTag("Trigger") ){
-			transform.GetComponent<Renderer>().material.color = Color.red;
+	void OnCollisionEnter(Collision collider) {
+		if ( collider.gameObject.CompareTag("Trigger") ){
+			transform.GetComponent<Renderer>().material.color = new Color( Random.value, Random.value, Random.value, 1.0f );
 		}
 	}
 }
---
+++ week04/Assets/Scripts/wall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Color tempColor = transform.GetComponent<Renderer> ().material.color;
+		tempColor.a = 0.5f;
+		transform.GetComponent<Renderer> ().material.color = tempColor;
+	}
+
+}
