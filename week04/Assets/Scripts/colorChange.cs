311f73c3fbf8e19eeb1bd4087998101461f0e53c
Kong Huang <kh1534@nyu.edu>
2015-09-24 21:09:16 -0400

    basically done, need to add bowl, music, camera

--- week04/Assets/Scripts/colorChange.cs
+++ week04/Assets/Scripts/colorChange.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class colorChange : MonoBehaviour {
+	bool changed = false;
+
 	void OnCollisionEnter(Collision collider) {
-		if ( collider.gameObject.CompareTag("Trigger") ){
+		if ( collider.gameObject.CompareTag("Trigger") && changed == false ){
 			transform.GetComponent<Renderer>().material.color = new Color( Random.value, Random.value, Random.value, 1.0f );
+			changed = true;
 		}
 	}
 }
---
+++ week04/Assets/Scripts/motorWorkaround.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class motorWorkaround : MonoBehaviour {
+
+	// constraint hacking because reverse motors are broken...
+	void OnCollisionEnter(Collision collider) {
+		if ( collider.gameObject.CompareTag("Trigger") ){
+			transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+		}
+	}
+}
