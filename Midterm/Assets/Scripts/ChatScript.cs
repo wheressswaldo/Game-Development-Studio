955c32a861b4c795978799184a40391b000d0034
Kong Huang <kh1534@nyu.edu>
2015-10-08 16:49:49 -0400

    made minimap better, added legend, fixed city flickering (wtf?)

--- Midterm/Assets/Scripts/ChatScript.cs
+++ Midterm/Assets/Scripts/ChatScript.cs
@@ -6,12 +6,23 @@ public class ChatScript : MonoBehaviour {
 	public Transform player;
 	public GameObject chatBox;
 	public GameObject station;
+	public GameObject minimapMarker;
+
+	private AudioSource source;
+
+	void Start() {
+		source = GetComponent<AudioSource> ();
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space) && (Vector3.Distance (transform.position, player.position) < 3f)) {
+			if (!source.isPlaying){
+				source.Play();
+			}
 			station.SetActive (true);
 			chatBox.SetActive (true);
+			minimapMarker.SetActive (false);
 		}
 
 		if (Vector3.Distance (transform.position, player.position) > 3f) {
---
+++ Midterm/Assets/Scripts/ExpandMinimap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExpandMinimap : MonoBehaviour {
+
+	public GameObject text;
+	public GameObject legend;
+
+	private Camera minimap;
+	private bool expanded;
+
+	// Use this for initialization
+	void Start () {
+		minimap = GetComponent<Camera> ();
+		expanded = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.M)) {
+			if (expanded) {
+				text.SetActive(true);
+				expanded = false;
+				legend.transform.Translate(700f, 150f, 0f);
+				minimap.rect = new Rect (0.74f, 0.6f, 0.4f, 0.4f);
+			}
+			else{
+				text.SetActive(false);
+				expanded = true;
+				legend.transform.Translate(-700f, -150f, 0f);
+				minimap.rect = new Rect (0f, 0.05f, 1f, 1f);
+			}
+		}
+	}
+}
